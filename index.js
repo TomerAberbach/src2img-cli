0781a91bb1da931fea2796e10d6d77d82c7abb1c
Tomer Aberbach <tomeraberbach@gmail.com>
2021-01-02 01:56:38 -0500

    feat: clean up package

--- index.js
+++ index.js
@@ -1,199 +1,220 @@
 #!/usr/bin/env node
+import { basename, join, resolve } from 'path'
+import os from 'os'
+import fs from 'fs-extra'
+import open from 'open'
+import program from 'commander'
+import inquirer from 'inquirer'
+import src2img from 'src2img'
+import filename2prism from 'filename2prism'
 
-const fs = require('fs-extra')
-const path = require('path')
-const os = require('os')
-const opn = require('opn')
+const configPath = join(os.homedir(), `.src2img`)
+const config = async () => {
+  try {
+    return await fs.readJson(configPath)
+  } catch {
+    await fs.outputJson(configPath, {})
+    return {}
+  }
+}
 
-const program = require('commander')
-const inquirer = require('inquirer')
-
-const src2img = require('src2img')
-const filename2prism = require('filename2prism')
-
-const configPath = path.join(os.homedir(), '.src2img')
-const config = (() => {
-  let json
-  return () => json ? Promise.resolve(json) : fs.readJson(configPath).then(a => {
-    json = a
-    return json
-  }, () => fs.outputJson(configPath, {}).then(() => {}))
-})()
-
-program.version(path.join(__dirname, 'package.json').version)
+program.name(`src2img`)
 
 program
-  .command('render <filenames...>')
-  .description('converts source code to high quality images')
-  .option('-o, --out <dir>', 'specifies an output directory', '.')
-  .option('-t, --type <type>', 'specifies an output file type (png or jpeg)', /^(png|jpeg)$/, 'png')
-  .option('-n, --port <number>', 'specifies a port number', parseInt, 8888)
-  .option('-p, --preset <name>', 'uses a preset')
-  .action((filenames, opts) => {
-    (opts.preset
-      ? config().then(json => opts.preset in json
-        ? json[opts.preset]
-        : Promise.reject(new Error('the provided preset does not exist'))
-      )
-      : inquirer.prompt([
+  .command(`render <filenames...>`)
+  .description(`converts source code to high quality images`)
+  .option(`-o, --out <dir>`, `specifies an output directory`, `.`)
+  .option(
+    `-t, --type <type>`,
+    `specifies an output file type (png or jpeg)`,
+    /^(?:png|jpeg)$/u,
+    `png`
+  )
+  .option(`-n, --port <number>`, `specifies a port number`, parseInt, 8888)
+  .option(`-p, --preset <name>`, `uses a preset`)
+  .action(async (filenames, { out, type, port, preset }) => {
+    const json = await config()
+
+    let answers
+    if (!preset) {
+      answers = await inquirer.prompt([
         {
-          type: 'input',
-          name: 'themePath',
-          message: 'Theme',
-          default: 'default'
+          type: `input`,
+          name: `themePath`,
+          message: `Theme`,
+          default: `default`
         },
         {
-          type: 'input',
-          name: 'fontFamily',
-          message: 'Font Family',
-          default: 'default'
+          type: `input`,
+          name: `fontFamily`,
+          message: `Font Family`,
+          default: `default`
         },
         {
-          type: 'input',
-          name: 'fontSize',
-          message: 'Font Size',
-          default: '20',
-          validate: answer => isNaN(parseFloat(answer)) ? 'the font size must be a number' : true,
+          type: `input`,
+          name: `fontSize`,
+          message: `Font Size`,
+          default: `20`,
+          validate: answer =>
+            isNaN(parseFloat(answer)) ? `the font size must be a number` : true,
           filter: answer => parseFloat(answer)
         },
         {
-          type: 'input',
-          name: 'fontSizeUnit',
-          message: 'Font Size Unit',
-          default: 'px',
-          validate: answer => /^(cm|mm|in|px|pt|pc|em|ex|ch|rem|vw|vh|vmin|vmax|%)$/i.test(answer) ? true : 'invalid font size unit',
-          filter: answer => answer.toLowerCase()
-        },
-        {
-          type: 'input',
-          name: 'padding',
-          message: 'Padding',
-          default: '20',
-          validate: answer => isNaN(parseFloat(answer)) ? 'the padding must be a number' : true,
-          filter: answer => parseFloat(answer)
-        },
-        {
-          type: 'input',
-          name: 'paddingUnit',
-          message: 'Padding Unit',
-          default: 'px',
-          validate: answer => /^(cm|mm|in|px|pt|pc|em|ex|ch|rem|vw|vh|vmin|vmax|%)$/i.test(answer) ? true : 'invalid padding unit',
-          filter: answer => answer.toLowerCase()
-        },
-        {
-          type: 'confirm',
-          name: 'transparent',
-          message: 'Transparent Background',
-          default: false
-        },
-        {
-          type: 'input',
-          name: 'background',
-          message: 'Background',
-          default: 'default',
-          when: answers => !answers.transparent
-        },
-        {
-          type: 'confirm',
-          name: 'save',
-          message: 'Save Preset',
-          default: false
-        }
-      ]).then(answers => {
-        if (answers.save) {
-          return (function f () {
-            return inquirer.prompt([{
-              type: 'input',
-              name: 'name',
-              message: 'Preset Name'
-            }]).then(({name}) =>
-              config().then(json => {
-                if (name in json) {
-                  return inquirer.prompt([{
-                    type: 'confirm',
-                    name: 'overwrite',
-                    message: 'The provided preset name already exists. Overwrite?',
-                    default: false
-                  }]).then(({overwrite}) => {
-                    if (overwrite) {
-                      json[name] = answers
-                      return fs.outputJson(configPath, json)
-                    } else {
-                      return inquirer.prompt([{
-                        type: 'confirm',
-                        name: 'change',
-                        message: 'Choose a different preset name?',
-                        default: false
-                      }]).then(({change}) => {
-                        if (change) {
-                          return f()
-                        }
-                      })
-                    }
-                  })
-                } else {
-                  json[name] = answers
-                  return fs.outputJson(configPath, json)
-                }
-              })
+          type: `input`,
+          name: `fontSizeUnit`,
+          message: `Font Size Unit`,
+          default: `px`,
+          validate: answer =>
+            /^(?:cm|mm|in|px|pt|pc|em|ex|ch|rem|vw|vh|vmin|vmax|%)$/iu.test(
+              answer
             )
-          })().then(() => answers)
-        } else {
-          return answers
+              ? true
+              : `invalid font size unit`,
+          filter: answer => answer.toLowerCase()
+        },
+        {
+          type: `input`,
+          name: `padding`,
+          message: `Padding`,
+          default: `20`,
+          validate: answer =>
+            isNaN(parseFloat(answer)) ? `the padding must be a number` : true,
+          filter: answer => parseFloat(answer)
+        },
+        {
+          type: `input`,
+          name: `paddingUnit`,
+          message: `Padding Unit`,
+          default: `px`,
+          validate: answer =>
+            /^(?:cm|mm|in|px|pt|pc|em|ex|ch|rem|vw|vh|vmin|vmax|%)$/iu.test(
+              answer
+            )
+              ? true
+              : `invalid padding unit`,
+          filter: answer => answer.toLowerCase()
+        },
+        {
+          type: `confirm`,
+          name: `transparent`,
+          message: `Transparent Background`,
+          default: false
+        },
+        {
+          type: `input`,
+          name: `background`,
+          message: `Background`,
+          default: `default`,
+          when: ({ transparent }) => !transparent
+        },
+        {
+          type: `confirm`,
+          name: `save`,
+          message: `Save Preset`,
+          default: false
         }
-      })
-    ).then(answers => {
-      ['themePath', 'fontFamily', 'background'].forEach(key => {
-        if (answers[key] === 'default') {
-          answers[key] = undefined
-        }
-      })
+      ])
 
-      if (typeof answers.themePath !== 'undefined') {
-        answers.themePath = path.resolve(answers.themePath)
+      if (answers.save) {
+        // eslint-disable-next-line no-constant-condition
+        while (true) {
+          const name = await inquirer.prompt([
+            {
+              type: `input`,
+              name: `name`,
+              message: `Preset Name`
+            }
+          ])
+
+          if (!(name in json)) {
+            json[name] = answers
+            await fs.outputJson(configPath, json)
+            break
+          }
+          const { overwrite } = await inquirer.prompt([
+            {
+              type: `confirm`,
+              name: `overwrite`,
+              message: `The provided preset name already exists. Overwrite?`,
+              default: false
+            }
+          ])
+
+          if (overwrite) {
+            json[name] = answers
+            await fs.outputJson(configPath, json)
+            break
+          }
+
+          const { change } = await inquirer.prompt([
+            {
+              type: `confirm`,
+              name: `change`,
+              message: `Choose a different preset name?`,
+              default: false
+            }
+          ])
+
+          if (!change) {
+            break
+          }
+        }
       }
+    } else if (preset in json) {
+      answers = json[preset]
+    } else {
+      throw new Error(`the provided preset does not exist`)
+    }
 
-      return Promise.all(filenames.map(filename => fs.readFile(filename)))
-        .then(contents =>
-          src2img(Object.assign({
-            src: filenames.map((filename, i) => {
-              let alias = filename2prism(filename)
+    ;[`themePath`, `fontFamily`, `background`].forEach(key => {
+      if (answers[key] === `default`) {
+        answers[key] = undefined
+      }
+    })
 
-              if (typeof alias === 'undefined') {
-                alias = 'clike'
-              } else if (Array.isArray(alias)) {
-                alias = alias[0]
-              }
+    if (typeof answers.themePath !== `undefined`) {
+      answers.themePath = resolve(answers.themePath)
+    }
 
-              return [
-                contents[i].toString(),
-                alias
-              ]
-            }),
-            type: opts.type,
-            port: opts.port
-          }, answers))
-        ).then(images =>
-          Promise.all(images.map((image, i) => {
-            fs.outputFile(path.join(opts.out, `${path.basename(filenames[i])}.${opts.type}`), image)
-          }))
-        )
-    }).catch(err => console.error(err.message))
+    const contents = await Promise.all(
+      filenames.map(filename => fs.readFile(filename))
+    )
+    const images = await src2img({
+      srcs: filenames.map((filename, i) => {
+        let alias = filename2prism(filename)
+
+        if (typeof alias === `undefined`) {
+          alias = `clike`
+        } else if (Array.isArray(alias)) {
+          alias = alias[0]
+        }
+
+        return [contents[i].toString(), alias]
+      }),
+      type,
+      port,
+      ...answers
+    })
+
+    await Promise.all(
+      images.map((image, i) =>
+        fs.outputFile(join(out, `${basename(filenames[i])}.${type}`), image)
+      )
+    )
   })
 
 program
-  .command('presets')
-  .description('lists saved presets')
-  .action(() =>
-    config()
-      .then(json => console.log(Object.keys(json).join('\n')))
-      .catch(err => console.error(err.message))
-  )
+  .command(`presets`)
+  .description(`lists saved presets`)
+  .action(async () => {
+    const json = await config()
+    console.log(Object.keys(json).join(`\n`))
+  })
 
 program
-  .command('open')
-  .description('opens the presets file')
-  .action(() => opn(configPath, {wait: false}))
+  .command(`open`)
+  .description(`opens the presets file`)
+  .action(() => open(configPath, { wait: false }))
 
 if (process.argv.slice(2).length === 0) {
   program.help()
---
+++ readme.md
@@ -0,0 +1,93 @@
+# src2img-cli
+
+[![NPM version](https://img.shields.io/npm/v/src2img-cli.svg)](https://www.npmjs.com/package/src2img-cli)
+
+> Converts source code to high quality images.
+
+## Install
+
+```sh
+$ npm i src2img-cli -g
+```
+
+If you encounter permission errors run the following instead:
+
+```sh
+$ sudo npm i src2img-cli -g --unsafe-perm=true --allow-root
+```
+
+The permission errors are related to [puppeteer](https://www.npmjs.com/package/puppeteer). See this [issue](https://github.com/GoogleChrome/puppeteer/issues/1597).
+
+## Usage
+
+Display the help information:
+
+```sh
+$ src2img
+```
+
+Output:
+
+```
+Usage: src2img [options] [command]
+
+Options:
+  -h, --help                       display help for command
+
+Commands:
+  render [options] <filenames...>  converts source code to high quality images
+  presets                          lists saved presets
+  open                             opens the presets file
+  help [command]                   display help for command
+```
+
+Display the render help information:
+
+```sh
+$ src2img help render
+```
+
+Output:
+
+```
+Usage: src2img render [options] <filenames...>
+
+converts source code to high quality images
+
+Options:
+  -o, --out <dir>      specifies an output directory (default: ".")
+  -t, --type <type>    specifies an output file type (png or jpeg)
+                       (default: "png")
+  -n, --port <number>  specifies a port number (default: 8888)
+  -p, --preset <name>  uses a preset
+  -h, --help           display help for command
+```
+
+Simple rendering example:
+
+```sh
+$ src2img render myfile.js
+```
+
+Follow the prompts as they pop up and feel free to save a preset! You can reuse a saved preset using the `-p` or `--preset` flag as shown above.
+
+Note that the best way to increase the resolution of the rendered image is to choose a high font size in the prompts.
+
+Some rendered code:
+
+![example](example.png)
+
+## Related
+
+- [filename2prism](https://www.npmjs.com/package/filename2prism)
+- [src2img](https://www.npmjs.com/package/src2img)
+
+## Contributing
+
+Stars are always welcome!
+
+For bugs and feature requests, [please create an issue](https://github.com/TomerAberbach/src2img-cli/issues/new).
+
+## License
+
+[MIT](https://github.com/TomerAberbach/src2img-cli/blob/master/license) © [Tomer Aberbach](https://github.com/TomerAberbach)
